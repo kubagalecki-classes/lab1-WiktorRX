34c5bb7a0a23e5add5fd620e3b8818ab0afc3320
WiktorRX <wiktorm@live.com>
2020-11-04 22:13:33 +0000

    test5

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -5,11 +5,11 @@ tor2D
 public:
     Wektor2D()
     {
-        x = 0;
-        y = 0;
+        x = 0.;
+        y = 0.;
         cout << "Stworzono wektor (3,4)\n";
     }
-    Wektor2D(int xx, int yy)
+    Wektor2D(double xx, double yy)
     {
         x = xx;
         y = yy;
@@ -17,7 +17,7 @@ public:
     }
     ~Wektor2D() { cout << "Z fartem (" << x << "," << y << ")\n"; }
 
-    double norm() { return sqrt((x * x) + (y * y)); }
+    
     void   print() { cout << "Wektor 2D: x=" << x << ", y=" << y << endl; }
     void   setX(int xxx) { x = xxx; };
     double getX() { return x; }
