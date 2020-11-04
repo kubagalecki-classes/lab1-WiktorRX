85965bd527059ffa6c0983053ae6a78ba610935c
WiktorRX <wiktorm@live.com>
2020-11-04 22:30:00 +0000

    test8

--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -26,6 +26,11 @@ Wektor2D operator+ (Wektor2D &v1, Wektor2D &v2)
   return Wektor2D{v1.x+v2.x, v1.y+v2.y};
 }
 
+double operator* (Wektor2D &v1, Wektor2D &v2)
+{
+  return v1.x*v2.x + v1.y*v2.y
+}
+
 private:
   double x;
   double y;
