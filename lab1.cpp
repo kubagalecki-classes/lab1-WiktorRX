9eee66132390bbd6c0ecce4ec3ef58bae69d1764
WiktorRX <wiktorm@live.com>
2020-11-04 20:40:38 +0000

    zad2

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,37 @@
+#include <cmath>
 #include <iostream>
+using namespace std;
+
+class Wektor2D
+{
+public:
+    Wektor2D()
+    {
+        x = 3;
+        y = 4;
+    }
+    Wektor2D(int xx, int yy = 1)
+    {
+        x = xx;
+        y = yy;
+    }
+    ~Wektor2D() { cout << "Z fartem (" << x << "," << y << ")\n"; }
+
+    int x;
+    int y;
+
+    double norm() { return sqrt((x * x) + (y * y)); }
+    void   print() { cout << "Wektor 2D: x=" << x << ", y=" << y << endl; }
+};
+
+class Informer
+{
+  public:
+  
+}
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D Gary, Harry(17), Merry(6, 9);
+    Gary.print();
 }
